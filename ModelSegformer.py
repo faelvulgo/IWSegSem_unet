25fb82d51a6d5d6424e7a6fcdb80797ab04f2e97
faelvulgo <rafael.bittencourt.2002@gmail.com>
2026-07-14 16:18:48 -0300

    Models modules add

---
+++ ModelSegformer.py
@@ -0,0 +1,21 @@
+import torch
+import segmentation_models_pytorch as smp
+
+#Define the encoder.
+ENCODER = "resnet34"
+
+# Define the weights for the encoder.
+ENCODER_WEIGHTS = "imagenet"
+ACTIVATION = "softmax"
+
+# Create the model using the specified encoder and weights.
+model = smp.Segformer(
+    encoder_name=ENCODER,
+    encoder_weights=ENCODER_WEIGHTS,
+    activation=ACTIVATION
+)
+
+# Normalize data.
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
+
+print(model)
